40ceee34c36f214e0c0301e0f1e6abb0af0b1367
Maksymilian Kalek <maks.kalek@gmail.com>
2021-08-19 22:38:39 +0200

    rendering to json

--- stxbooks/stxbooks/settings.py
+++ stxbooks/stxbooks/settings.py
@@ -28,7 +28,6 @@ DEBUG = True
 REST_FRAMEWORK = {
     'DEFAULT_RENDERER_CLASSES': [
         'rest_framework.renderers.JSONRenderer',
-        'rest_framework.renderers.BrowsableAPIRenderer',
     ]
 }
 
